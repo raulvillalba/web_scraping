f4fcf71d79e33aa31c6cf9d003c88abe7814f96e
raulvillalba <raulvillalba@uoc.edu>
2021-04-06 20:12:34 +0200

    Get by year beta

--- main.py
+++ main.py
@@ -49,11 +49,12 @@ def obtener_descripcion_y_premios(href):
 
     return descripcion, listaPremios
 
+print("\nBienvenido a la aplicación de web scraping de Jose Cano y Raul Villaba")
+print("Con esta aplicación puedes obtener los datos de 200 peliculas del año que tu decidas")
+print("Introducir año:")
+año = input()
 
-url = 'https://www.filmaffinity.com/es/topcat.php?id=new_th_es'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-cards = soup.find_all("div", class_='movie-card movie-card-22')
+
 
 titulo = []
 referencia = []
@@ -63,29 +64,49 @@ descripcion = []
 calificacion = []
 listaPremios = []
 listaCriticas = []
-for card in cards:
-    poster = card.find("div", class_='mc-poster')
-    titulo.append(poster.a['title'])
-    referencia.append(poster.a['href'])
-    calificacion.append(card.find('div', class_='avg-rating').text)
-    duracion.append(card.find('div', class_='duration').text)
-    imagen.append(card.find('img')['src'])
 
-    descripcion.append(obtener_descripcion_y_premios(referencia[-1])[0])
-    listaPremios.append(obtener_descripcion_y_premios(referencia[-1])[1])
+for i in range(10):
 
-    # pagina=requests.get(referencia[-1])
-    soup = BeautifulSoup(requests.get(referencia[-1]).content, 'html.parser')
+    url = "https://www.filmaffinity.com/es/advsearch.php?page=" + str(i+1) + "stext=&stype%5B%5D=title&country=&genre=&fromyear=" + str(
+        año) + "&toyear=" + str(año)
+    print(url)
 
-    box = soup.find('div', attrs={'id': 'movie-reviews-box'})
 
-    listaCriticas.append(obtener_criticas(box.a['href']))
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    cards = soup.find_all("div", class_='movie-card movie-card-1')
+
+
+    for card in cards:
+        poster = card.find("div", class_='mc-poster')
+
+        titulo.append(poster.a['title'])
+
+        referencia.append(poster.a['href'])
+        calificacion.append(1)
+        duracion.append(1)
+        #calificacion.append(card.find('div', class_='avg-rating').text)
+        #duracion.append(card.find('div', class_='duration').text)
+        imagen.append(card.find('img')['src'])
+
+        descripcion.append(obtener_descripcion_y_premios(referencia[-1])[0])
+        listaPremios.append(obtener_descripcion_y_premios(referencia[-1])[1])
+
+        # pagina=requests.get(referencia[-1])
+        soup = BeautifulSoup(requests.get(referencia[-1]).content, 'html.parser')
+
+        box = soup.find('div', attrs={'id': 'movie-reviews-box'})
+
+        listaCriticas.append(obtener_criticas(box.a['href']))
 
 caracteristicas = (
 'titulo', 'referencia', 'duracion', 'imagen', 'descripcion', 'calificacion', 'ListaPemios', 'listaCriticas')
+
 df = pd.DataFrame(
     list(zip(titulo, referencia, duracion, imagen, descripcion, calificacion, listaPremios, listaCriticas)),
     columns=(caracteristicas))
-df.to_csv('filmaffinity.csv', index=False)
+
+df.to_csv('filmaffinity_' + str(año) +'.csv', index=False)
 
 # films=pd.read_csv('D:/WebScraping/filmaffinity.csv')
